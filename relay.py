3e2788453dd11ac92489ff611acab4de1cc97a56
Marie Ballenger <marie.ballenger@gmail.com>
2023-11-08 23:54:13 -0800

    Add files via upload
    setting up classes

---
+++ relay.py
@@ -0,0 +1,13 @@
+class Relay:
+    def __init__(self, ageGroup, whichRelay, relayTime, swimmerList, nationalRecord):
+        self.ageGroup = ageGroup
+        self.whichRelay = whichRelay
+        self.relayTime = relayTime
+        self.swimmerList = swimmerList
+        self.nationalRecord = nationalRecord
+
+
+    def createRelays():
+        // ages 18 - 24 must be in 18-24 relays
+        // sort swimmers by time50Free
+        
---
+++ relaySwimmer.py
@@ -0,0 +1,4 @@
+class RelaySwimmer(Swimmer):
+    def __init__(self, name, time50Free, ageGroup, whichRelay):
+        self.ageGroup = ageGroup
+        self.whichRelay = whichRelay
