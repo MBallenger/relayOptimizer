25ff2bfa2c92b2d763c96bbe4d8038f6b047f4d2
Marie Ballenger <marie.ballenger@gmail.com>
2023-11-09 00:09:22 -0800

    fixing merge conflicts

--- swimmer.py
+++ swimmer.py
@@ -5,30 +5,15 @@ class Swimmer:
         self.sex = sex
         self.time50Free = time50Free
 
-<<<<<<< HEAD
-marieB = Swimmer("Marie Ballenger", 37, "F", 24.00)
-laureenW = Swimmer("Laureen Welting", 50, "F", 27.00)
-courtneyM = Swimmer("Courtney Monsees", 35, "F", 24.00)
-feliciaL = Swimmer("Felica Lee", 30, "F", 23.00)
-deirdreC = Swimmer("Dierdre Clute", 28, "F", 24.00)
-catL = Swimmer("Cat Ladd", 27, "F", 24.50)
-naokoW = Swimmer("Naoko Watanabe", 40, "F", 25.00)
-allisonA = Swimmer("Allison Arnold", 32, "F", 25.00)
-kendylS = Swimmer("Kendyl Stewart", 24, "F", 23.00)
-ericaS = Swimmer("Erica Schreer", 36, "F", 25.00)
-laurenG = Swimmer("Lauren Green", 23, "F", 23.00)
-
-=======
-    marieB = Swimmer("Marie Ballenger", 37, F, 24.00)
-    laureenW = Swimmer("Laureen Welting", 50, F, 27.00)
-    courtneyM = Swimemr("Courtney Monsees", 35, F, 24.00)
-    feliciaL = Swimmer("Felica Lee", 30, F, 23.00)
-    deirdreC = Swimmer("Dierdre Clute", 28, F, 24.00)
-    catL = Swimmer("Cat Ladd", 27, F, 24.50)
-    naokoW = Swimmer("Naoko Watanabe", 40, F, 25.00)
-    allisonA = Swimmer("Allison Arnold", 32, F, 25.00)
-    kendylS = Swimmer("Kendyl Stewart", 24, F, 23.00)
-    ericaS = Swimmer("Erica Schreer", 36, F, 25.00)
-    laurenG = Swimmer("Lauren Green", 23, F, 23.00)
+marieB = Swimmer("Marie Ballenger", 37, F, 24.00)
+laureenW = Swimmer("Laureen Welting", 50, F, 27.00)
+courtneyM = Swimmer("Courtney Monsees", 35, F, 24.00)
+feliciaL = Swimmer("Felica Lee", 30, F, 23.00)
+deirdreC = Swimmer("Dierdre Clute", 28, F, 24.00)
+catL = Swimmer("Cat Ladd", 27, F, 24.50)
+naokoW = Swimmer("Naoko Watanabe", 40, F, 25.00)
+allisonA = Swimmer("Allison Arnold", 32, F, 25.00)
+kendylS = Swimmer("Kendyl Stewart", 24, F, 23.00)
+ericaS = Swimmer("Erica Schreer", 36, F, 25.00)
+laurenG = Swimmer("Lauren Green", 23, F, 23.00)
     
->>>>>>> b6dfb43 (setting up classes)
