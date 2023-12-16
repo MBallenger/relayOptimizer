f6d5808e02e6f907012a14bb50e8359153db171f
Marie Ballenger <marie.ballenger@gmail.com>
2023-12-15 22:00:25 -0800

    Relays sorted by ageGroup and relayTime

--- relay.py
+++ relay.py
@@ -1,5 +1,8 @@
 import itertools
 from itertools import combinations
+from itertools import groupby
+import operator
+from operator import itemgetter
 
 
 class Swimmer:
@@ -30,20 +33,43 @@ class Swimmer:
 def createRelayCombinations():
     swimList = Swimmer.createWSwimmerList()
     relays = list(itertools.combinations(swimList, 4))
+    global relaysDictList
+    relaysDictList = []
     for i in range(len(relays)):
         print(i, end=" ")
         relay = relays[i]
         minAge = relay[0].age
         estRelayTime = 0
+        ageGroup = 0
+        relayDict = {}
         for j in range(len(relay)):
             relaySwimmer = relay[j]
+            relayDict["swimmer" + str(j+1)] = relaySwimmer.name
+            relayDict["swimmer" + str(j+1) + "Time"] = relaySwimmer.time50Free
             estRelayTime += relaySwimmer.time50Free
             if relaySwimmer.age < minAge:
                 minAge = relaySwimmer.age
-            print(relaySwimmer.name)
-            #print(relaySwimmer.time50Free)
-        print("min age: " + str(minAge))
-        print("relay time: " + str(estRelayTime))
-    
-createRelayCombinations()
+        if minAge < 25:
+            ageGroup = 18
+        else:
+            ageGroup = 5 * round(minAge/5)
+        relayDict["ageGroup"] = ageGroup
+        relayDict["relayTime"] = estRelayTime
+        relaysDictList.append(relayDict)
+    return relaysDictList
 
+def sortRelays():   
+    createRelayCombinations()
+    # Sort relays by ageGroup.
+    relaysDictListSorted = sorted(relaysDictList, key = itemgetter("ageGroup"))
+ 
+    # Display data grouped by ageGroup
+    sortedRelays = {}
+    for key, value in groupby(relaysDictListSorted, key = itemgetter("ageGroup")):
+        print("\n")
+        sortedRelays['%s' % key] = []
+        for k in value:
+            sortedRelays['%s' % key].append(k)
+        sortedRelays['%s' % key] = sorted(sortedRelays['%s' % key], key = itemgetter("relayTime"))
+
+sortRelays()
